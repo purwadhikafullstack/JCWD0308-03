666fe8e788285088388d451ed137ecc6666d3965
sallysbury <avandasallysbury@gmail.com>
2024-06-10 15:32:25 +0700

    feat: api

---
+++ apps/api/src/routers/user.routers.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { UserTransaction } from "../controllers/user/user.transaction.controllers";
+
+export class UserTransactionRouter {
+    private router: Router
+    private userTransaction : UserTransaction
+
+    constructor() {
+        this.userTransaction = new UserTransaction()
+        this.router = Router()
+        this.initializeRoutes()
+    }
+    private initializeRoutes(): void{
+        this.router.post('/reservation', this.userTransaction.createReservation)
+        this.router.patch('/reservation/:reservationId/upload-proof', this.userTransaction.uploadPaymentProof)
+        this.router.get('/users/:userId/reservations', this.userTransaction.getUserReservations)
+        this.router.patch('/reservations/:reservationId/cancel', this.userTransaction.cancelReservation)
+    }
+
+    getRouter() {
+        return this.router
+    }
+}
