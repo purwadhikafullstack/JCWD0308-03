5b363d14aedd0bc63d9d177c5bd8d11ff42a99c0
Hani Nur <haninrry@gmail.com>
2024-06-11 14:01:38 +0700

    feat: (api) add helpers, propertyController

--- apps/api/src/routers/api.router.ts
+++ apps/api/src/routers/api.router.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { PropertyRouter } from './property.router';
 
 export class ApiRouter {
+  private propertyRouter: PropertyRouter;
   private router: Router;
 
   constructor() {
     this.router = Router();
+    this.propertyRouter = new PropertyRouter();
     this.initializeRoutes();
   }
 
   private initializeRoutes(): void {
+    this.router.use('/properties', this.propertyRouter.getRouter());
   }
   getRouter(): Router {
     return this.router;
