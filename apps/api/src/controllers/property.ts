2b25d4086dd4dfc2bfe8d7c7998a49c88aa61807
Hani Nur <haninrry@gmail.com>
2024-06-12 21:10:32 +0700

    feat: (web) modify search, add categories

--- apps/api/src/controllers/property.ts
+++ apps/api/src/controllers/property.ts
@@ -5,17 +5,38 @@ import { responseError } from '@/helpers/responseError';
 export class PropertyController {
   async getProperties(req: Request, res: Response) {
     try {
-      const properties = await prisma.property.findMany();
-      res.status(200).json({
-        status: 'ok',
-        properties,
+      const { name = '', category = '', sortBy, sortOrder } = req.query;
+  
+      const filterOptions: any = name || category ? { } : undefined;
+      if (name) filterOptions.name = { contains: name.toString().toLocaleLowerCase()};
+      if (category) filterOptions.category = category.toString();
+  
+      const orderBy = sortBy && sortOrder ? { [sortBy.toString()]: sortOrder.toString().toUpperCase() === 'DESC' ? 'desc' : 'asc' } : undefined;
+  
+      const properties = await prisma.property.findMany({
+        where: filterOptions,
+        orderBy,
       });
+  
+      res.status(200).json({ status: 'ok', properties });
     } catch (error) {
       responseError(res, error);
     }
   }
 
-  async createProperty(req: Request, res: Response) {}
+  async createProperty(req: Request, res: Response) {
+    try {
+      const createdProperty = await prisma.property.create({
+        data: {
+          ...req.body,
+        },
+      });
+
+      res.status(201).json(createdProperty);
+    } catch (error) {
+      responseError(res, error);
+    }
+  }
 
   async getPropertyById(req: Request, res: Response) {}
 }
